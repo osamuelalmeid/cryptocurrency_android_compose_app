8c0e750d4f41bbb1fd1cd98acb680a38fb62eac1
Samuel <samuelalmeida95@outlook.com>
2021-10-26 13:16:51 -0300

    Create coin list screen

--- app/src/main/java/com/samuelalmeida/cryptocurrencyandroidcompose/common/Constants.kt
+++ app/src/main/java/com/samuelalmeida/cryptocurrencyandroidcompose/common/Constants.kt
@@ -3,4 +3,6 @@ package com.samuelalmeida.cryptocurrencyandroidcompose.common
 object Constants {
     const val BASE_URL = "https://api.coinpaprika.com/"
     const val PARAM_COIN_ID = "coinId"
+    const val ACTIVE = "Ativo"
+    const val INACTIVE = "Inativo"
 }
